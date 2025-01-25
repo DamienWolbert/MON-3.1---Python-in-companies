9862688ab8c6623f2be9455678b73c9ebedddb63
DamienWolbert <damienwolbert@gmail.com>
2025-01-25 20:01:54 +0100

    maj

--- A lancer.py
+++ A lancer.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from openpyxl import load_workbook
 
-path_excel = "C:/Users/damie/Documents/GitHub/MON-3.1---Python-in-companies/Tests 2.xlsm"
+path_excel = r"C:\Users\damie\Documents\GitHub\MON-3.1---Python-in-companies\Tests 2.xlsx"
 nom_feuille = "Test 2"
 
 #Ouverture de la feuille
-fichier = load_workbook(path_excel)
+fichier = load_workbook(path_excel,keep_vba=True)
 feuille = fichier[nom_feuille]
 
 # Complétion des données
